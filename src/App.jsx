66a85d4b3e56b838e60df93f2a366906ab42d5b3
Dev Pipalia <devpipalia003@gmail.com>
2024-06-13 16:03:17 +0530

    top buttons - cities

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react"
+import TopButtons from "./components/TopButtons"
+import Inputs from "./components/Inputs"
+
+
 
 function App() {
-
-
   return (
     <>
-      <p className="text-3xl">hello</p>
+      <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
+        <TopButtons/>
+        <Inputs/>
+      </div>
     </>
   )
 }
