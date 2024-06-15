f18580c0ca402ee723eac8863d5372acd51c7fe7
Dev Pipalia <devpipalia003@gmail.com>
2024-06-16 02:44:00 +0530

    search bar implemented

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
-import { BiSearch,BiCurrentLocation } from 'react-icons/bi'
+import React, { useState } from "react";
+import { BiSearch, BiCurrentLocation } from "react-icons/bi";
 
-
-
-const Inputs = () => {
+const Inputs = ({ setQuery, setUnits }) => {
+  const [city, setCity] = useState("");
+  const handleSearchClick=()=>{
+    if(city!==" ") setQuery({q:city})
+  }
   return (
     <>
-        <div className='flex flex-row justify-center my-6'>
-        <div className='flex flex-row justify-center w-3/4 items-center space-x-4'>
-            <input type='text' placeholder='search by city' className='text-gray-600 text-light p-2 w-full shadow-xl capitalize focus:outline-none placeholder:lowercase'/>
-            <BiSearch size={30} className='cursor-pointer transition ease-out hover:scale-125' color='white'/>
-            <BiCurrentLocation size={30} className='cursor-pointer transition ease-out hover:scale-125' color='white'/>
+      <div className="flex flex-row justify-center my-6">
+        <div className="flex flex-row justify-center w-3/4 items-center space-x-4">
+          <input
+            type="text"
+            placeholder="search by city"
+            className="text-gray-600 text-light p-2 w-full shadow-xl capitalize focus:outline-none placeholder:lowercase"
+            value={city}
+            onChange={(e)=>setCity(e.target.value)}
+          />
+          <BiSearch
+            size={30}
+            className="cursor-pointer transition ease-out hover:scale-125"
+            color="white"
+            onClick={handleSearchClick}
+          />
+          <BiCurrentLocation
+            size={30}
+            className="cursor-pointer transition ease-out hover:scale-125"
+            color="white"
+          />
         </div>
 
-        <div className='flex flex-row w-1/4 items-center justify-center'> 
-            <button className='text-2xl font-medium transition ease-out hover:scale-125 text-white' >°C</button>
-            <button className='text-2xl font-medium mx-1  text-white'>|</button>
-            <button className='text-2xl font-medium transition ease-out hover:scale-125  text-white' >°F</button>
+        <div className="flex flex-row w-1/4 items-center justify-center">
+          <button className="text-2xl font-medium transition ease-out hover:scale-125 text-white">
+            °C
+          </button>
+          <button className="text-2xl font-medium mx-1  text-white">|</button>
+          <button className="text-2xl font-medium transition ease-out hover:scale-125  text-white">
+            °F
+          </button>
         </div>
-        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Inputs
+export default Inputs;
--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -1,24 +1,42 @@
 import React from "react";
 
-const TopButtons = () => {
+const TopButtons = ({ setQuery }) => {
+  const cities = [
+    {
+      id: 1,
+      name: "London",
+    },
+    {
+      id: 2,
+      name: "Sydney",
+    },
+    {
+      id: 3,
+      name: "Tokyo",
+    },
+    {
+      id: 4,
+      name: "Paris",
+    },
+    {
+      id:5,
+      name:'Toronto',
+    },
+  ];
+  
+
   return (
     <>
-      <div className="flex justify-around items-center my-6 ">
-        <button className="text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in">
-          London
-        </button>
-        <button className="text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in">
-          Sydney
-        </button>
-        <button className="text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in">
-          Tokyo
-        </button>
-        <button className="text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in">
-          Mumbai
-        </button>
-        <button className="text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in">
-          New York
-        </button>
+      <div className="flex justify-around items-center my-6 text-white">
+        {cities.map((city) => (
+          <button
+            key={city.id}
+            className="text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in"
+            onClick={() => setQuery({ q: city.name })}
+          >
+            {city.name}
+          </button>
+        ))}
       </div>
     </>
   );
