a6d91d957f781736cc1d043f97bad7fe6dea4eb1
Dev Pipalia <devpipalia003@gmail.com>
2024-06-15 12:39:51 +0530

    fetched current weather data

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ import Inputs from "./components/Inputs"
 import Time from "./components/Time"
 import Temparature from "./components/Temparature"
 import Forecast from "./components/Forecast"
+import getWeatherData from "./services/WeatherService"
+import getFormattedWeatherData from "./services/WeatherService"
 
 
 function App() {
+  const getWeather=async()=>{
+    const data = await getFormattedWeatherData({q:"berlin"})
+    console.log(data)
+  }
+
+  getWeather();
   return (
     <>
       <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
